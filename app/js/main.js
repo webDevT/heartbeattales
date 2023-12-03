211f4efcd3ba96674fa5cedc7795a92ec4fdfd7d
webDevT <karinadesyatnik27@gmail.com>
2023-12-03 12:48:43 +0200

    updates

--- app/js/main.js
+++ app/js/main.js
@@ -9,3 +9,26 @@ $(window).scroll(function() {
 $('.menu-btn').click(function(){
     $('.menu').slideToggle();
 })
+$(window).scroll(function() {
+    if ($(this).scrollTop() > 1){
+    $('.fixed-footer').addClass("sticky");
+    }
+    else{
+    $('.fixed-footer').removeClass("sticky");
+    }
+    });
+
+    var $element = $('.footer');
+    let counter = 0;
+$(window).scroll(function() {
+    var scroll = $(window).scrollTop() + $(window).height();
+    var offset = $element.offset().top
+  
+    if (scroll > offset) {
+      $('.fixed-footer').removeClass('sticky');
+      counter = 1;
+    
+  } else {
+    $('.fixed-footer').addClass('sticky');
+  }
+  });
